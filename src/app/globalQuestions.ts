aaf1bb0eb27597eaedd034a7c17c1f32923d8df0
Ben Flavell <ben.t.flavell@gmail.com>
2020-09-17 21:55:46 +1000

    changed reward message

--- src/app/globalQuestions.ts
+++ src/app/globalQuestions.ts
@@ -5,26 +5,26 @@ export const questions = {
     '1': {
         'question': "how old is yoda when he finally succumbs to the force?",
         'answers': ["900", "nine hundred"],
-        'number': "4"
+        'number': "Correct! The first digit is 4."
     },
     '2': {
         'question': "who is the only non-jedi in the original star wards trilogy to use a lightsaber? ",
         'answers': ["han solo"],
-        'number': "1"
+        'number': "Correct! The second digit is 1."
     },
     '3': {
         'question': "what sort of business did luke's uncle owen run?",
         'answers': ["moisture farm", "moisture farmer"],
-        'number': "1"
+        'number': "Correct! The third digit is 1."
     },
     '4': {
         'question': "what creature lives in the garbage compactor of the original death star?",
         'answers': ["dianoga"],
-        'number': "behind the door"
+        'number': "Correct! The key is behind the door."
     },
     'bonus': {
         'question': "where was luke going into?",
         'answers': ["tosche station"],
-        'number': "https://youtu.be/QM0BUgCL2vY"
+        'number': "Correct! Here are your directions: https://youtu.be/QM0BUgCL2vY"
     }
 };
